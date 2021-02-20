55a7465b07e670ee2b09b85e255a75ff1559946d
Hung Tang <hungtang@hotmail.com>
2021-02-19 16:00:18 -0800

    completed the task edit/save feature and resetting it back to normal

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,14 +19,30 @@ var taskFormHandler = function (event) {
 
     formEl.reset();
     
+    var isEdit = formEl.hasAttribute("data-task-id");
+
     //package up data as an object
-    var taskDataObj = {
+    /*var taskDataObj = {
         name: taskNameInput,
         type: taskTypeInput
-    };
+    }; */
 
     //send it as an argument to createTaskEl
-    createTaskEl(taskDataObj);
+    //Put this below var isEdit = .....
+    //has data attribute, so get task id and call function to complete edit process
+    if (isEdit) {
+        var taskId = formEl.getAttribute("data-task-id");
+        completeEditTask(taskNameInput, taskTypeInput, taskId);
+    }
+    //no data attribute, so create object as normal and pass to createTaskEl function
+    else {
+        var taskDataObj = {
+            name: taskNameInput,
+            type: taskTypeInput
+        };
+
+        createTaskEl(taskDataObj);
+    }
 };
 
 var createTaskEl = function(taskDataObj) {
@@ -97,6 +113,20 @@ var createTaskActions = function(taskId) {
 
     return actionContainerEl;
 };
+
+var completeEditTask = function(taskName, taskType, taskId) {
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+    //set new values
+    taskSelected.querySelector("h3.task-name").textContent = taskName;
+    taskSelected.querySelector("span.task-type").textContent = taskType;
+
+    alert("Task Updated!");
+
+    //reset form & remove task ID and change button back to normal
+    formEl.removeAttribute("data-task-id");
+    document.querySelector("#save-task").textContent = "Add Task";
+}
 
 var editTask = function(taskId) {
     console.log("editing task # " + taskId);
